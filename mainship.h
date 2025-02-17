6f6166e1d4aeec99564ba2bc4903a2218cd60126
Anna Trifonova <annatrifonova737@gmail.com>
2025-02-17 12:47:40 +0300

    Add files via upload
    main class

---
+++ mainship.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class ships1 {
+public:
+    double speed;
+    double length;
+    double width;
+    double displacement;
+    int crew;
+};
