574619072b7bac090a9f8e3bfc06dcdb9fdb6e8d
Anna Trifonova <annatrifonova737@gmail.com>
2025-02-17 12:53:30 +0300

    Add files via upload
    class passenger

---
+++ passenger.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <mainship.h>
+
+using namespace std;
+
+class passenger: public ship  {
+public:
+    int numberpass;
+    int deck ;
+    int lifeboat;
+};
