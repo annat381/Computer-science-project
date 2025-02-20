bbfea61fcc0cbcacf3297d94e7d12dde26e7c01c
Anna Trifonova <annatrifonova737@gmail.com>
2025-02-20 11:13:06 +0300

    main

---
+++ main.h
@@ -0,0 +1,19 @@
+#include " mainship.h"
+#include "icebreaker.h"
+#include "passenger.h"
+#include "submarine.h"
+#include "tanker.h"
+
+int main() {
+    icebreaker icebreaker_1;
+    tanker tanker_1;
+    submarine  submarine_1;
+    passenger passenger_1;
+
+    tanker_1.makeSound();
+    icebreaker_1.makeSound();
+    passengerr_1.makeSound();
+    submarine_1.makeSound();
+
+    return 0;
+}
